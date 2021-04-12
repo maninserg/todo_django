da78cb4452905689ee397f7824943d82f4c67f78
maninserg <manin.serg@gmail.com>
2021-04-13 00:14:35 +0300

    Change #1 functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,20 @@
 from selenium import webdriver
+import unittest
 
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
 
-assert "Congratulations!" in browser.title
+    def setUp(self):
+        """ open firefox """
+        self.browser = webdriver.Firefox()
+
+    def test_can_start_a_list_and_retrive_it_later(self):
+        self.browser.get("http:/localhost:8000")
+        self.assertIn("ToDo", self.browser.title)
+
+    def tearDown(self):
+        """ close firefox """
+        self.browser.quit()
+
+if __name__ == "__main__":
+    unittest.main()
