476ad2ad4f11cd8f3ed7341a31be3c5565475e70
maninserg <manin.serg@gmail.com>
2021-04-13 19:09:20 +0300

    Add useful things to home page template

--- functional_tests.py
+++ functional_tests.py
@@ -27,7 +27,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # Test that a task can be typed
         inputbox.send_keys('Buy peacock feathers')
-        inputbox.send_keys(Keys.Enter)
+        inputbox.send_keys(Keys.ENTER)
 
         # Time sleep for refreshing home page
         time.sleep(1)
@@ -36,7 +36,8 @@ class NewVisitorTest(unittest.TestCase):
         # there is task was typed before in the table
         table = self.browser.find_element_by_id('id_list_table')
         rows = self.browser.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row == '1: Buy peacock feathers' for row in rows))
+        self.assertTrue(any(row == '1: Buy peacock feathers' for row in rows),
+                        "New item of list wasn't added to list")
 
     def tearDown(self):
         """ close firefox """
