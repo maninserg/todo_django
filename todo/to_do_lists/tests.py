8628f28fa0cee480eaca363d610e2a2758729c5c
maninserg <manin.serg@gmail.com>
2021-04-13 18:44:04 +0300

    Add djngo's test client

--- todo/to_do_lists/tests.py
+++ todo/to_do_lists/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from django.urls import resolve
-from django.http import HttpRequest
 
 from to_do_lists.views import home_page
 
@@ -12,9 +11,5 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf-8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>ToDo lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('')
+        self.assertTemplateUsed(response, 'home.html')
