cd204f676d89e68a2a05b16ace699de9608900c8
maninserg <manin.serg@gmail.com>
2021-04-14 13:22:48 +0300

    Add create new items

--- todo/to_do_lists/views.py
+++ todo/to_do_lists/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 
 def home_page(request):
+    if request.method == 'POST':
+        return render (request, 'home.html',
+                       {'new_item_text': request.POST['item_text']})
     return render (request, 'home.html')
