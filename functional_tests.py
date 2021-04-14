a58aeb6c12c94335802950419d6abe45ca38425b
maninserg <manin.serg@gmail.com>
2021-04-14 13:23:49 +0300

    Refactoring functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,12 @@ class NewVisitorTest(unittest.TestCase):
         """ open firefox """
         self.browser = webdriver.Firefox()
 
+    def check_for_row_in_list_table(self,row_text):
+        # Check string in list table
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrive_it_later(self):
 
         # Test that 'ToDo' is in the title
@@ -34,10 +40,20 @@ class NewVisitorTest(unittest.TestCase):
 
         # Test that a table with tasks is on the home page and
         # there is task was typed before in the table
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = self.browser.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row == '1: Buy peacock feathers' for row in rows),
-                        "New item of list wasn't added to list")
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        # Test that a task can be typed
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Make mouschk by peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Time sleep for refreshing home page
+        time.sleep(1)
+
+        # Test that a table with tasks is on the home page and
+        # there is task was typed before in the table
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.assertIn('2: Make mouschk by peacock feathers')
 
     def tearDown(self):
         """ close firefox """
