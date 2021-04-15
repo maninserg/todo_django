de4536c71574457c6b451c648b4e990455dcd588
maninserg <manin.serg@gmail.com>
2021-04-15 19:52:24 +0300

    Add url for adding item to existing lists

--- todo/to_do_lists/tests.py
+++ todo/to_do_lists/tests.py
@@ -79,17 +79,23 @@ class NewListTest(TestCase):
 
     def test_can_save_a_POST_request(self):
 
-        response = self.client.post('/lists/new',
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(f'/lists/{correct_list.id}/add_item',
                                     data={'item_text': 'A new list item'})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
+        self.assertEqual(new_item.list, correct_list)
 
-    def test_redirects_after_POST(self):
+    def test_redirects_to_list_views(self):
 
-        response = self.client.post('/lists/new',
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(f'/lists/{correct_list.id}/add_item',
                                     data={'item_text': 'A new list item'})
 
         new_list = List.objects.first()
-        self.assertRedirects(response, f'/lists/{new_list.id}/')
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
